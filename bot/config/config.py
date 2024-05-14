b082a5db84c40028d5ec41bd194207d39014526c
vherasymenko <manqarium@gmail.com>
2024-05-14 18:14:52 +0300

    [Fix] update config

--- bot/config/config.py
+++ bot/config/config.py
@@ -8,10 +8,10 @@ class Settings(BaseSettings):
     API_HASH: str
     USE_PROXY_FROM_FILE: bool = False
 
-    MIN_AVAILABLE_ENERGY: int = 200
+    MIN_AVAILABLE_ENERGY: int = 10
     SLEEP_BY_MIN_ENERGY: int = 200
-    RANDOM_TAPS_COUNT: list[int] = [100, 190]
-    SLEEP_BETWEEN_TAP: list[int] = [5, 10]
+    RANDOM_TAPS_COUNT: list[int] = [5, 9]
+    SLEEP_BETWEEN_TAP: list[int] = [10, 15]
     AUTO_UPGRADE: bool = True
 
 
